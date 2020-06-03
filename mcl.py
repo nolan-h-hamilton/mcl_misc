6546d6d06e9a1f6fbd935aade134fa2c388c5ef9
nolan-h-hamilton <nhh.git@gmail.com>
2020-06-02 20:20:56 -0400

    add size_distr_dict{} to cluster_features() return value

--- mcl.py
+++ mcl.py
@@ -13,6 +13,8 @@ def cluster_features(mcl_output_file, species, normalize=False):
                 in `mcl_output_file`
             'cluster_sizes': a list containing sizes of all clusters ob-
                 served
+            'size_distr_dict': dict containing keys for mean, min, and
+                 max of observed cluster sizes
             'singletons': a list of single-sequence clusters observed
             'num_clusters': total number of clusters observed
             'clusters': list of lists containing observed clusters
@@ -27,26 +29,38 @@ def cluster_features(mcl_output_file, species, normalize=False):
     cluster_sizes = []
     singletons = []
     num_clusters = 0
+    size_distr_dict = {'mean_clstr_size': 0.0, 'min_clstr_size': 0.0, 'max_clstr_size': 0.0}
+    
     with open (mcl_output_file, 'r') as f:
         for line in f:
             # each 'lcl|' denotes cluster
             clusters = line.split('lcl|')
             for cluster in clusters:
 
-                # Note: this may not be needed, but may avoid potentially
+                # get tab-separated seqs in cluster
+                # and record cluster
+                try:
+                    cluster = cluster.split('\t')[0:-1]
+                except AttributeError:
+                    #blank line?
+                    continue
+                clusters.append(cluster)
+
+                # this counter may not be needed, but may avoid potentially
                 # expensive calls to len(clusters) in future. Keep for now.
                 num_clusters += 1
 
-                # get tab-separated seqs in cluster
-                # and record cluster
-                cluster = cluster.split('\t')[0:-1]
-                clusters.append(cluster)
-
-                # record size of each cluster
-                cluster_sizes.append(len(cluster))
+                # compute cluster size features
+                size = len(cluster)
+                size_distr_dict['mean_clstr_size'] += size
+                if size_distr_dict['min_clstr_size'] > size:
+                    size_distr_dict['min_clstr_size'] = size
+                if size_distr_dict['max_clstr_size'] < size:
+                    size_distr_dict['max_clstr_size'] = size
+                cluster_sizes.append(size)
 
                 # record singleton clusters
-                if len(cluster) == 1:
+                if size == 1:
                     singletons.append(cluster[0])
 
                 # determine species of seqs in cluster
@@ -59,11 +73,12 @@ def cluster_features(mcl_output_file, species, normalize=False):
     # True,  divide frequency by num_clusters so that the value in dict
     # represents what fraction of clusters the species was present in
     if normalize:
-        for key in species_frequency.keys():
+        for key in species_frequency:
             species_frequency[key] /= num_clusters
 
     return {'species_frequency': species_frequency,
             'cluster_sizes': cluster_sizes,
+            'size_distr_dict': size_distr_dict,
             'singletons': singletons,
             'num_clusters': num_clusters,
             'clusters': clusters}
